daabaa62fd706203d469dcffc8ef57521d5d53ab
dharmendras-tw <dharmendra.s@thoughtworks.com>
2023-11-05 21:05:51 +0530

    First commit

---
+++ src/app/Home.js
@@ -0,0 +1,12 @@
+
+function Home() {
+  return (
+    <div>
+      <section>
+        <h2>Welcome to the Redux Essentials example app!</h2>
+     </section>
+    </div>
+  );
+}   
+
+export default Home;
